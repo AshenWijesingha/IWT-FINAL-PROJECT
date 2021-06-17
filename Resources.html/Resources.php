0d4a3be50198de19664555c90149a635125b6fdd
Ashen Wijesingha <a.d.wijesingha@gmail.com>
2021-06-17 21:27:48 +0530

    Initial commit

---
+++ Resources.html/Resources.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <title>Simple website</title>
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="bootstrap.css">
+	<link rel="stylesheet" href="images.css">
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
+  </head>
+
+  <body id="bg">
+<div>
+<!-- Start Nav Bar --> 
+<nav class="navbar" style="position: fixed; width: 100%;">
+  <div class="navbar__container">
+    <a href="\iwtProject\index.php" id="navbar__logo">Resource Booking System</a>
+    <div class="navbar__toggle" id="mobile-menu">
+      <span class="bar"></span>
+      <span class="bar"></span>
+      <span class="bar"></span>
+      <span class="bar"></span>
+    </div>
+
+    <ul class="navbar__menu">
+
+      <li class="navbar__item">
+        <input type="text" placeholder="Search.." id="searchTxt" style="width: 250px;">
+      </li>
+
+      <li class="navbar__item">
+        <a href="\iwtProject\index.php" class="navbar__links" id="home-page">Home</a>
+      </li>
+      <li class="navbar__item">
+        <a href="\iwtProject\Available Resources.html\AvailableResources.php" class="navbar__links" id="AvailableResources-page">Available Resources</a>
+      </li>
+      <li class="navbar__item">
+        <a href="#" class="navbar__links" id="Resources-page">Resources</a>
+      </li>
+      <li class="navbar__item">
+        <a href="\iwtProject\Daily Shedule.html\DailyShedule.php" class="navbar__links" id="DailyShedule-page">Daily Shedule</a>
+      </li>
+      <li class="navbar__item">
+        <a href="\iwtProject\AboutUs.html\AboutUs.php" class="navbar__links" id="AboutUs-page">About Us</a>
+      </li>
+      <li class="navbar__item">
+        <a href="\iwtProject\Contact Us.html\ContactUs.php" class="navbar__links" id="ContactUs-page">Contact Us</a>
+      </li>
+      <li class="navbar__btn">
+        <a href="\iwtProject\Login.html\Login.php" class="button1" id="LogIn" style="width:10rem;">Log In</a>
+      </li> 
+    </ul>
+  </div>
+</nav>
+<!-- End Nav Bar -->
+
+<!-- Body Start -->
+<br>
+<br>
+<hr>
+<br>
+
+<h1 style="text-align: center;">Resources</h1>
+
+<!-- <div id="devCont_top" style="height: 5rem;">  
+  <div id="devCont_middle"> 
+    <div id="devCont_bottom">   
+      <div class="topic">
+        <div style="align-items: center; width: auto; height: auto;">
+          <img src="\iwtProject\\iwtProject\RBS Images\RBS Images\\iwtProject\RBS Images\RBS Images\Araliya_1.jpg" alt="Araliya_1" style="border: 2rem; padding-left: 5%; height: fit-content;" > 
+        </div>
+      </div>      
+    </div>        
+  </div>      
+</div> -->
+ 
+<hr>
+<br>
+
+
+<!-- <div id="devCont_top" style="height: 5rem;">  
+  <div id="devCont_middle"> 
+    <div id="devCont_bottom">   
+      <div class="topic">
+        <div style="align-items: center; width: auto; height: auto;">
+        <img src="\iwtProject\\iwtProject\RBS Images\RBS Images\\iwtProject\RBS Images\RBS Images\Araliya_1.jpg" class="recImg" alt="Araliya Banquet Hall">
+        <img src="\iwtProject\\iwtProject\RBS Images\RBS Images\\iwtProject\RBS Images\RBS Images\Araliya_2.jpg" class="recImg" alt="Araliya Banquet Hall">
+        <img src="\iwtProject\\iwtProject\RBS Images\RBS Images\\iwtProject\RBS Images\RBS Images\Araliya_3.jpg" class="recImg" alt="Araliya Banquet Hall">
+        </div>
+      </div>      
+    </div>        
+  </div>      
+</div> -->
+
+<div id="devCont_middle" style="display: inline-flexbox;"> 
+  <div style="align-items: center; width: auto; height: auto;">
+    <div id="devCont_middle" style="height: 1450rem;">      
+
+      <img src="\iwtProject\RBS Images\RBS Images\Araliya_1.jpg" class="recImg" alt="Araliya Banquet Hall"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Araliya_2.jpg" class="recImg" alt="Araliya Banquet Hall"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Araliya_3.jpg" class="recImg" alt="Araliya Banquet Hall"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Jasmin_1.jpg" class="recImg" alt="Jasmin Banquet Hall"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Jasmin_2.jpg" class="recImg" alt="Jasmin Banquet Hall"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Jasmin_3.jpg" class="recImg" alt="Jasmin Banquet Hall"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Main_1.jpg" class="recImg" alt="Main Auditorium"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Main_2.jpg" class="recImg" alt="Main Auditorium"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Mini_1.jpg" class="recImg" alt="Mini Auditorium"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Mini_2.jpg" class="recImg" alt="Mini Auditorium"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\LMR_1_1.jpg" class="recImg" alt="Luxury Meeting Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\LMR_1_2.jpg" class="recImg" alt="Luxury Meeting Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\LMR_2_1.jpg" class="recImg" alt="Luxury Meeting Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\LMR_2_2.jpg" class="recImg" alt="Luxury Meeting Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\NMR_1.png" class="recImg" alt="Meeting Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\NMR_2.jpg" class="recImg" alt="Meeting Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Visiting.png" class="recImg" alt="Visiting Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Single 01_1.jpg" class="recImg" alt="Single Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Single 01_2.jpg" class="recImg" alt="Single Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Single 02_1.jpg" class="recImg" alt="Single Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Single 02_2.jpg" class="recImg" alt="Single Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Double 01_1.jpg" class="recImg" alt="Double Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Double 01_2.jpg" class="recImg" alt="Double Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Double 02_1.jpg" class="recImg" alt="Double Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Double 02_2.jpg" class="recImg" alt="Double Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Triple 01_1.jpg" class="recImg" alt="Triple Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Triple 01_2.jpg" class="recImg" alt="Triple Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Triple 02_1.jpg" class="recImg" alt="Triple Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Triple 02_2.jpg" class="recImg" alt="Triple Room"><br>
+
+      <p></p>
+
+    </div>
+  </div>
+</div>
+        
+<br>
+<hr>
+<br>
+ 
+<!-- Body End -->
+
+
+
+<!-- Footer Start -->
+<footer class="site_footer" id="footer" style="font-family: 'Courier New', Courier, monospace; position:absolute; width:100%; height:fit-content;"> <!--1933-->
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 col-md-6" style="bottom: 0%;">
+        <h6 style="font-family: 'Courier New', Courier, monospace; font-size: 2rem;">About</h6>
+        <p class="text-justify" style="font-family: 'Courier New', Courier, monospace; font-size: larger;">Welcome to newly designed website resource booking system. Resource booking system for the management and schedule of your facilities and resources. This web site is a faster, cleaner, and secure more personal website, especially this web site designed to make your booking experience well. 
+        Resource booking that provides organizations with complete over the resources they wish to be able to book. Also, the goal is help user to get more direct bookings easily & contribute to growing their business.
+        </p>
+      </div>
+
+      <div class="col-xs-6 col-md-3">
+        <h6 style="font-family: 'Courier New', Courier, monospace; font-size: 1rem;">Categories</h6>
+        <ul class="footer-links" style="font-family: 'Courier New', Courier, monospace;">
+          <li><a href="\iwtProject\index.php" style="text-decoration: none;">Home</a></li>
+          <li><a href="\iwtProject\Available Resources.html\AvailableResources.php" style="text-decoration: none;">Available Resources</a></li>
+          <li><a href="\iwtProject\Resources.html\Resources.php" style="text-decoration: none;">Resources</a></li>
+          <li><a href="\iwtProject\Daily Shedule.html\DailyShedule.php" style="text-decoration: none;">Daily Shedule</a></li>
+          <li><a href="#" style="text-decoration: none;">About Us</a></li>
+          <li><a href="\iwtProject\Contact Us.html\ContactUs.php" style="text-decoration: none;">Contact Us</a></li>
+        </ul>
+      </div>
+
+      <div class="col-xs-6 col-md-3">
+        <h6 style="font-family: 'Courier New', Courier, monospace; font-size: 1rem;">Quick Links</h6>
+        <ul class="footer-links">
+          <li><a href="#">Contact Over facebook</a></li>
+          <li><a href="#">Contact Over twitter</a></li>
+          <li><a href="#">Contact Over Instergram</a></li>
+          <li><a href="#">Contact Over Whatsapp</a></li>
+          <li><a href="#">Contact Over Massanger</a></li>
+        </ul>
+      </div>
+    </div>
+    <hr>
+  </div>
+  
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-sm-6 col-xs-12">
+        <p class="copyright_text">Copyright &copy; 2021 All Rights Reserved by 
+     <a href="#">Group GLB_3.2_11</a>.
+        </p>
+      </div>
+
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <ul class="social-icons">
+          <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+          <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+          <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+          <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+<!-- Footer End -->
+</div>
+  </body>
+</html>
