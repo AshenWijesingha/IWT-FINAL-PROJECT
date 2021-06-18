f94e25a5b17373eeb9d79e1874ceb9d74703628d
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-06-18 11:58:33 +0530

    Ahamad Available reso Done

--- Available Resources.html/AvailableResources.php
+++ Available Resources.html/AvailableResources.php
@@ -4,68 +4,491 @@
 
 <!-- Body Start -->
 <br>
+<br>
 <hr>
 <br>
 
-<br>
-<h1 style="text-align: center; position:relative; width: 100%;">Available Resources</h1>
-<br>
+<h1 style="text-align: center;">Resources</h1>
 
-<div id="devCont_top" style="height: 5rem;">  
+<!-- <div id="devCont_top" style="height: 5rem;">  
   <div id="devCont_middle"> 
     <div id="devCont_bottom">   
       <div class="topic">
-
+        <div style="align-items: center; width: auto; height: auto;">
+          <img src="../\iwtProject\RBS Images\RBS Images\\iwtProject\RBS Images\RBS Images\Araliya_1.jpg" alt="Araliya_1" style="border: 2rem; padding-left: 5%; height: fit-content;" > 
+        </div>
       </div>      
     </div>        
-  </div>       
-</div>
-
-<br>
+  </div>      
+</div> -->
+ 
 <hr>
 <br>
 
 
-<div id="devCont_middle" style="height: 5rem;">         
+<!-- <div id="devCont_top" style="height: 5rem;">  
+  <div id="devCont_middle"> 
+    <div id="devCont_bottom">   
+      <div class="topic">
+        <div style="align-items: center; width: auto; height: auto;">
+        <img src="\iwtProject\\iwtProject\RBS Images\RBS Images\\iwtProject\RBS Images\RBS Images\Araliya_1.jpg" class="recImg" alt="Araliya Banquet Hall">
+        <img src="\iwtProject\\iwtProject\RBS Images\RBS Images\\iwtProject\RBS Images\RBS Images\Araliya_2.jpg" class="recImg" alt="Araliya Banquet Hall">
+        <img src="\iwtProject\\iwtProject\RBS Images\RBS Images\\iwtProject\RBS Images\RBS Images\Araliya_3.jpg" class="recImg" alt="Araliya Banquet Hall">
+        </div>
+      </div>      
+    </div>        
+  </div>      
+</div> 
+
+<div id="devCont_middle" style="display: inline-flexbox;"> 
+  <div style="align-items: center; width: auto; height: auto;">
+    <div id="devCont_middle" style="height: 1450rem;">      
+
+      <img src="\iwtProject\RBS Images\RBS Images\Araliya_1.jpg" class="recImg" alt="Araliya Banquet Hall"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Araliya_2.jpg" class="recImg" alt="Araliya Banquet Hall"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Araliya_3.jpg" class="recImg" alt="Araliya Banquet Hall"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Jasmin_1.jpg" class="recImg" alt="Jasmin Banquet Hall"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Jasmin_2.jpg" class="recImg" alt="Jasmin Banquet Hall"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Jasmin_3.jpg" class="recImg" alt="Jasmin Banquet Hall"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Main_1.jpg" class="recImg" alt="Main Auditorium"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Main_2.jpg" class="recImg" alt="Main Auditorium"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Mini_1.jpg" class="recImg" alt="Mini Auditorium"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Mini_2.jpg" class="recImg" alt="Mini Auditorium"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\LMR_1_1.jpg" class="recImg" alt="Luxury Meeting Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\LMR_1_2.jpg" class="recImg" alt="Luxury Meeting Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\LMR_2_1.jpg" class="recImg" alt="Luxury Meeting Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\LMR_2_2.jpg" class="recImg" alt="Luxury Meeting Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\NMR_1.png" class="recImg" alt="Meeting Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\NMR_2.jpg" class="recImg" alt="Meeting Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Visiting.png" class="recImg" alt="Visiting Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Single 01_1.jpg" class="recImg" alt="Single Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Single 01_2.jpg" class="recImg" alt="Single Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Single 02_1.jpg" class="recImg" alt="Single Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Single 02_2.jpg" class="recImg" alt="Single Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Double 01_1.jpg" class="recImg" alt="Double Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Double 01_2.jpg" class="recImg" alt="Double Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Double 02_1.jpg" class="recImg" alt="Double Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Double 02_2.jpg" class="recImg" alt="Double Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Triple 01_1.jpg" class="recImg" alt="Triple Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Triple 01_2.jpg" class="recImg" alt="Triple Room"><br>
+
+      <p></p>
+
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Triple 02_1.jpg" class="recImg" alt="Triple Room"><br>
+      <img src="\iwtProject\RBS Images\RBS Images\Rooms\Triple 02_2.jpg" class="recImg" alt="Triple Room"><br>
+
+      <p></p>
+
+    </div>
+  </div>
 </div>
         
 <br>
 <hr>
 <br>
-
-<div id="devCont_bottom" style="height: 5rem;">         
-</div>
-
-<br>
-<hr>
-<br>
-
-<div id="devCont_bottom" style="height: 5rem;">         
-</div>
-
-<br>
-<hr>
-<br>
-
-<div id="devCont_bottom" style="height: 5rem;">         
-</div>
-
-<br>
-<hr>
-<br>
-
-<div id="devCont_bottom" style="height: 5rem;">         
-</div>
-
-<br>
-<hr>
-<br>
-
-<div id="devCont_bottom" style="height: 5rem;">         
-</div>
-
-<br> 
+ 
 <!-- Body End -->
+
+
+
+
+
+<div class="hero-container">
+    <div class="main-container">
+      <div class="poster-container">
+        <img src="../RBS Images/RBS Images/Main_1.jpg" class="recImg" alt="Main Auditorium"></a>
+      </div>
+      <div class="ticket-container">
+        <div class="ticket__content">
+          <h4 class="ticket__movie-title">Main Auditorium </h4>
+          <p class="ticket__movie-slogan">
+            • The Main Auditorium is an ideal place for Conferences, Seminars, Ceremonies, Consents and any other Programs. 
+            <br>
+• The seating capacity is for 2000.
+ <br>
+• Fully air-conditioned hall with a stage area.
+ <br>
+• Special Lighting and Raised Floor.
+ <br>
+• 5 HD projectors with separate white screens.
+ <br>
+• Latest surround sound system gives you a high-quality sound experience.
+ <br> 
+• Separate USB charging outlets for every chair.
+ <br>
+• In addition, we offer Wi-Fi, Parking, Separate VIP Parking facilities & refreshments.
+ <br>
+• Real-time availability detection, up-to-date floorplans.
+          </p>
+          <p class="ticket__current-price">Rs. 115000.00</p>
+          
+          <button class="ticket__buy-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="main-container">
+      <div class="poster-container">
+        <a href="#"> <img src="../RBS Images/RBS Images/Mini_1.jpg" class="recImg" alt="Mini Auditorium"></a>
+      </div>
+      <div class="ticket-container">
+        <div class="ticket__content">
+          <h4 class="ticket__movie-title">Mini Auditorium</h4>
+          <p class="ticket__movie-slogan">• The Mini Auditorium is an ideal place for Seminars, Round Table Discussions, Book/Product Launches and any other Small-Scale Programs. 
+<br>
+• The seating capacity is for 400.
+<br>
+• Fully air-conditioned hall with a stage area.
+<br>
+• We offer audio-visual facilities for our Mini Auditorium.
+<br>
+• In addition, we offer Wi-Fi, Parking, Separate VIP Parking facilities & refreshments.
+<br>
+• Real-time availability detection, up-to-date floorplans.
+</p>
+          <p class="ticket__current-price">Rs. 60000.00</p>
+          <p class="ticket__old-price">Rs. 72000.00</p>
+          <button class="ticket__buy-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+
+
+<div class="main-container">
+      <div class="poster-container">
+        <a href="#"><img src="../RBS Images/RBS Images/Araliya_1.jpg" class="recImg" alt="Araliya Banquet Hall"></a>
+      </div>
+      <div class="ticket-container">
+        <div class="ticket__content">
+          <h4 class="ticket__movie-title">Araliya Banquet Hall</h4>
+          <p class="ticket__movie-slogan">• The Aralya Banquet Hall is large enough to hold up to 500 guests.
+            <br>
+• We provide all of our guest access to our audio-visual equipment free of charge.
+<br>
+• Onsite attractive kitchen area is equipped with a stove & oven, ice maker, refrigerator.
+<br>
+• Luxurious indoor environment and attractive outdoor environment.
+<br>
+• Attractive moldings and ceilings & velvet carpeted floors.
+<br>
+• In addition, we offer Wi-Fi, Parking, Separate VIP Parking facilities.
+<br>
+• Real-time availability detection, up-to-date floorplans.
+</p>
+          <p class="ticket__current-price">Rs. 60000.00</p>
+          <p class="ticket__old-price">Rs. 66000.00</p>
+          <button class="ticket__buy-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+
+
+
+<div class="main-container">
+      <div class="poster-container">
+        <a href="#"><img src="../RBS Images/RBS Images/Jasmin_1.jpg" class="recImg" alt="Jasmin Banquet Hall"></a>
+      </div>
+      <div class="ticket-container">
+        <div class="ticket__content">
+          <h4 class="ticket__movie-title">Jasmin Banquet Hall</h4>
+          <p class="ticket__movie-slogan">•  The Aralya Banquet Hall is large enough to hold up to 500 guests.
+            <br>
+• We provide all of our guest access to our audio-visual equipment free of charge.
+<br>
+• Onsite attractive kitchen area is equipped with a stove & oven, ice maker, refrigerator.
+<br>
+• Luxurious indoor environment and attractive outdoor environment.
+<br>
+• Attractive moldings and ceilings & velvet carpeted floors.
+<br>
+• In addition, we offer Wi-Fi, Parking, Separate VIP Parking facilities.
+<br>
+• Real-time availability detection, up-to-date floorplans. 
+</p>
+          <p class="ticket__current-price">Rs. 55000.00</p>
+          <p class="ticket__old-price">Rs. 57000.00</p>
+          <button class="ticket__buy-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+
+
+
+<div class="main-container">
+      <div class="poster-container">
+        <a href="#"><img src="../RBS Images/RBS Images/LMR_1_1.jpg" class="recImg" alt="Luxury Meeting Room"></a>
+      </div>
+      <div class="ticket-container">
+        <div class="ticket__content">
+          <h4 class="ticket__movie-title">Luxury Meeting Room</h4>
+          <p class="ticket__movie-slogan">• 15 comfortable chairs.
+            <br>
+• 60” FHD TV & projector with large screen.
+<br>
+• High speed internet & Wi-Fi facilities.
+<br>
+• High quality sound system with microphones.
+<br>
+• Camera and white board (Optional)
+<br>
+• Access to communal lounge, breakout areas, kitchens and courtyard.
+<br>
+• Secured with CCTV and alarm system.
+<br>
+• Comfortable waiting area.
+<br>
+• In addition, we offer Wi-Fi, Parking, Separate VIP Parking facilities & refreshments.
+<br>
+• Real-time availability detection, up-to-date floorplans.
+</p>
+          <p class="ticket__current-price">Rs. 35000.00</p>
+          <p class="ticket__old-price">Rs. 40000.00</p>
+          <button class="ticket__buy-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+
+
+
+<div class="main-container">
+      <div class="poster-container">
+        <a href="#"><img src="../RBS Images/RBS Images/LMR_2_1.jpg" class="recImg" alt="Luxury Meeting Room"></a>
+      </div>
+      <div class="ticket-container">
+        <div class="ticket__content">
+          <h4 class="ticket__movie-title">Luxury Meeting Room</h4>
+          <p class="ticket__movie-slogan">• 15 comfortable chairs.
+            <br>
+• 60” FHD TV & projector with large screen.
+<br>
+• High speed internet & Wi-Fi facilities.
+<br>
+• High quality sound system with microphones.
+<br>
+• Camera and white board (Optional)
+<br>
+• Access to communal lounge, breakout areas, kitchens and courtyard.
+<br>
+• Secured with CCTV and alarm system.
+<br>
+• Comfortable waiting area.
+<br>
+• In addition, we offer Wi-Fi, Parking, Separate VIP Parking facilities & refreshments.
+<br>
+• Real-time availability detection, up-to-date floorplans.</p>
+          <p class="ticket__current-price">Rs. 28000.00</p>
+          <p class="ticket__old-price">Rs. 32000.00</p>
+          <button class="ticket__buy-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+
+
+
+<div class="main-container">
+      <div class="poster-container">
+        <a href="#"><img src="../RBS Images/RBS Images/NMR_1.png" class="recImg" alt="Meeting Room"></a>
+      </div>
+      <div class="ticket-container">
+        <div class="ticket__content">
+          <h4 class="ticket__movie-title">Meeting Room</h4>
+          <p class="ticket__movie-slogan">• 15 comfortable chairs.
+            <br>
+• Projector with large screen.
+<br>
+• High quality sound system with microphones.
+<br>
+• White board (Optional)
+<br>
+• Access to communal lounge, breakout areas and courtyard.
+<br>
+• Secured with CCTV and alarm system.
+<br>
+• In addition, we offer Wi-Fi, Parking, Separate VIP Parking facilities & refreshments.
+<br>
+• Real-time availability detection, up-to-date floorplans.
+</p>
+          <p class="ticket__current-price">Rs. 20000.00</p>
+          <p class="ticket__old-price">Rs. 23000.00</p>
+          <button class="ticket__buy-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+
+
+
+<div class="main-container">
+      <div class="poster-container">
+        <a href="#"><img src="../RBS Images/RBS Images/Visiting.png" class="recImg" alt="Visiting Room"></a>
+      </div>
+      <div class="ticket-container">
+        <div class="ticket__content">
+          <h4 class="ticket__movie-title">Visiting Room.</h4>
+          <p class="ticket__movie-slogan">• Combatable room with A/C & Heating facilities.
+            <br>
+• Attached bathroom.
+<br>
+• Television with satellite connection.
+<br>
+• Dining table with chairs. 
+<br> 
+• We can arrange little extras such as daily newspapers, magazines and any special dietary requirements.
+<br>
+• In addition, we offer Wi-Fi, Parking, Separate VIP Parking facilities & refreshments.
+<br>
+• Real-time availability detection, up-to-date floorplans.
+
+</p>
+          <p class="ticket__current-price">Rs. 12000.00</p>
+          <p class="ticket__old-price">Rs. 15000.00</p>
+          <button class="ticket__buy-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="main-container">
+      <div class="poster-container">
+        <img src="../RBS Images/RBS Images/Rooms\Single 01_1.jpg" class="recImg" alt="Single Room"></a>
+      </div>
+      <div class="ticket-container">
+        <div class="ticket__content">
+          <h4 class="ticket__movie-title">Single Room</h4>
+          <p class="ticket__movie-slogan">• High-quality furnishings with opulent, expensive touches, attention to aesthetic detail, a quiet room with fresh air.
+            <br>
+• A single bed with a good mattress & high-quality sheets and pillows. 
+<br>
+• Separate shower/bathtub
+<br>
+• A desk and a reading chair.
+<br>
+• A good size FHD TV with satellite connection.
+<br>
+• Effective heating/AC system.
+<br>
+• In addition, we offer Wi-Fi, Parking, Separate VIP Parking facilities & bed tea.
+<br>
+• Real-time availability detection, up-to-date floorplans.
+</p>
+          <p class="ticket__current-price">Rs. 14000.00</p>
+          <p class="ticket__old-price">Rs. 15000.00</p>
+          <button class="ticket__buy-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+
+
+
+
+<div class="main-container">
+      <div class="poster-container">
+        <a href="#"><img src="../RBS Images/RBS Images/Rooms\Double 01_1.jpg" class="recImg" alt="Double Room"></a>
+      </div>
+      <div class="ticket-container">
+        <div class="ticket__content">
+          <h4 class="ticket__movie-title">Double Room</h4>
+          <p class="ticket__movie-slogan">• High-quality furnishings with opulent, expensive touches, attention to aesthetic detail, a quiet room with fresh air.
+            <br>
+• Two single beds with a good mattress & high-quality sheets and pillows. 
+<br>
+• Separate shower/bathtub
+<br>
+• A desk and a reading chair.
+<br>
+• A good size FHD TV with satellite connection.
+<br>
+• Effective heating/AC system.
+<br>
+• In addition, we offer Wi-Fi, Parking, Separate VIP Parking facilities & bed tea.
+<br>
+• Real-time availability detection, up-to-date floorplans.
+</p>
+          <p class="ticket__current-price">Rs. 18000.00</p>
+          <p class="ticket__old-price">Rs. 19500.00</p>
+          <button class="ticket__buy-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+
+
+
+
+<div class="main-container">
+      <div class="poster-container">
+        <a href="#"><img src="../RBS Images/RBS Images/Rooms\Triple 01_1.jpg" class="recImg" alt="Triple Room"></a>
+      </div>
+      <div class="ticket-container">
+        <div class="ticket__content">
+          <h4 class="ticket__movie-title">Triple Room</h4>
+          <p class="ticket__movie-slogan">• High-quality furnishings with opulent, expensive touches, attention to aesthetic detail, a quiet room with fresh air.
+           <br>
+• Three twin beds with a good mattress & high-quality sheets and pillows. 
+<br>
+• Separate shower/bathtub
+<br>
+• A desk and a reading chair.
+<br>
+• A good size FHD TV with satellite connection.
+<br>
+• Effective heating/AC system.
+<br>
+• In addition, we offer Wi-Fi, Parking, Separate VIP Parking facilities & bed tea.
+<br>
+• Real-time availability detection, up-to-date floorplans.
+</p>
+          <p class="ticket__current-price">Rs. 20000.00</p>
+          <p class="ticket__old-price">Rs. 22500.00</p>
+          <button class="ticket__buy-btn">Book Now</button>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+  </div>
 
 
 
