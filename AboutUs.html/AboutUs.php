3c8d18bdff0d498d5dc33c1499e5cae18761d586
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2021-06-18 15:28:23 +0530

    About Done

---
+++ AboutUs.html/AboutUs.php
@@ -0,0 +1,32 @@
+<?php
+  include '../HeaderFooter/header.php'
+?>
+
+<!-- Body Start -->
+<br>
+<hr>
+<br>
+
+<br>
+<h1 style="text-align: center; position: relative; width: 100%;">About Us</h1>
+<br>
+
+<div id="devCont_top" style="height: 53rem">  
+<p style="font-family:'Courier New', Courier, monospace; font-size:1.5rem; color:white; padding-top:1rem; padding-bottom:1rem;">Welcome to newly designed website resource booking system. Resource booking system for the management and schedule of your facilities and resources. This web site is a faster, cleaner, and secure more personal website, especially this web site designed to make your booking experience well.<br><br>
+  
+  Resource booking that provides organizations with complete over the resources they wish to be able to book. Also, the goal is help user to get more direct bookings easily & contribute to growing their business.<br><br>
+  
+  The biggest benefit of a booking system for consumers is convenience. User can search anything in their desired booking, compare prices, check availability for their selected date or dates and finally make the booking online - all in the comfort from their home, in just a few clicks. The program automatically calculates the total amount of booking. ABC can be used to booking meeting rooms, presentation rooms, auditorium whatever you want in booking system.  ABC is a system for multisite booking rooms are shown in a slide-by-slide view.<br><br>
+  
+  Another advantage with this system is that consumers can access a lot of information about their booking and room, hall, auditorium including images, list of location on map etc. to help them decide if they want to book that booking or not. With a mobile app, they can do all those things on the go. Use the resource booking system user can save so much time for his reservation<br>
+  </p>
+
+</div>
+<br> 
+<!-- Body End -->
+
+
+
+<?php
+  include_once '../HeaderFooter/footer.php'
+?>
